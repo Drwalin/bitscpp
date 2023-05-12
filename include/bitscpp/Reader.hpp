e8aed184954261e2c14e611f0fc2e19b959dad10
Drwalin <marek.zalewski.drwalin@gmail.com>
2023-05-12 23:50:56 +0200

    Someparts works.

---
+++ include/bitscpp/Reader.hpp
@@ -0,0 +1,338 @@
+/*
+ *  This file is part of ICon6.
+ *  Copyright (C) 2023 Marek Zalewski aka Drwalin
+ *
+ *  ICon6 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ICon6 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef BIT_SERIALIZATION_CPP_READER_HPP
+#define BIT_SERIALIZATION_CPP_READER_HPP
+
+#include <cinttypes>
+#include <cstring>
+
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+
+namespace bitscpp {
+	
+	
+	class ByteReader;
+	
+	template<typename T, typename... Args>
+	inline ByteReader& op(ByteReader& reader, T& data, Args... args) {
+		data.__ByteStream_op(reader, args...);
+		return reader;
+	}
+	
+	namespace impl {
+		template<typename T, typename... Args>
+		static inline ByteReader& __op_ptr(ByteReader& reader, T* data, Args... args) {
+			op(reader, *data, args...);
+			return reader;
+		}
+		
+		template<typename T, typename... Args>
+		static inline ByteReader& __op_ref(ByteReader& reader, T& data, Args... args) {
+			op(reader, data, args...);
+			return reader;
+		}
+	}
+	
+	class ByteReader {
+	public:
+		
+		template<typename T, typename... Args>
+		inline ByteReader& op(T* data, Args... args) {
+			impl::__op_ptr(*this, data, args...);
+			return *this;
+		}
+		
+		template<typename T, typename... Args>
+		inline ByteReader& op(T& data, Args... args) {
+			impl::__op_ref(*this, data, args...);
+			return *this;
+		}
+		
+	public:
+		
+		inline ByteReader(const uint8_t* buffer, uint32_t size) : buffer(buffer), size(size), offset(0) {}
+		
+		
+		
+		// NULL-terminated string
+		inline ByteReader& op(std::string& str);
+		// constant size byte array
+		inline ByteReader& op(uint8_t* data, uint32_t bytes);
+		inline ByteReader& op(int8_t* data, uint32_t bytes);
+		
+		// uint32_t size preceeds size of binary data
+		inline ByteReader& op(std::vector<uint8_t>& binary);
+		
+		
+		inline ByteReader& op(uint8_t& v,  uint32_t bytes);
+		inline ByteReader& op(uint16_t& v, uint32_t bytes);
+		inline ByteReader& op(uint32_t& v, uint32_t bytes);
+		inline ByteReader& op(uint64_t& v, uint32_t bytes);
+		inline ByteReader& op(int8_t& v,   uint32_t bytes);
+		inline ByteReader& op(int16_t& v,  uint32_t bytes);
+		inline ByteReader& op(int32_t& v,  uint32_t bytes);
+		inline ByteReader& op(int64_t& v,  uint32_t bytes);
+		inline ByteReader& op(uint8_t& v,  int32_t bytes) { return op(v, (uint32_t)bytes); }
+		inline ByteReader& op(uint16_t& v, int32_t bytes) { return op(v, (uint32_t)bytes); }
+		inline ByteReader& op(uint32_t& v, int32_t bytes) { return op(v, (uint32_t)bytes); }
+		inline ByteReader& op(uint64_t& v, int32_t bytes) { return op(v, (uint32_t)bytes); }
+		inline ByteReader& op(int8_t& v,   int32_t bytes) { return op(v, (uint32_t)bytes); }
+		inline ByteReader& op(int16_t& v,  int32_t bytes) { return op(v, (uint32_t)bytes); }
+		inline ByteReader& op(int32_t& v,  int32_t bytes) { return op(v, (uint32_t)bytes); }
+		inline ByteReader& op(int64_t& v,  int32_t bytes) { return op(v, (uint32_t)bytes); }
+		
+		inline ByteReader& op(uint8_t& v);
+		inline ByteReader& op(uint16_t& v);
+		inline ByteReader& op(uint32_t& v);
+		inline ByteReader& op(uint64_t& v);
+		inline ByteReader& op(int8_t& v);
+		inline ByteReader& op(int16_t& v);
+		inline ByteReader& op(int32_t& v);
+		inline ByteReader& op(int64_t& v);
+		
+		
+		inline ByteReader& op(float& v);
+		inline ByteReader& op(double& v);
+		
+		inline ByteReader& op(float& v, float min, float max, uint32_t bytes);
+		inline ByteReader& op(double& v, double min, double max, uint32_t bytes);
+		
+		inline ByteReader& op(float& v, float offset, float min, float max, uint32_t bytes);
+		inline ByteReader& op(double& v, double offset, double min, double max, uint32_t bytes);
+		
+	public:
+		
+		template<typename T, typename... Args>
+		inline ByteReader& op(T* data, uint32_t elements, Args... args) {
+			for(uint32_t i=0; i<elements; ++i)
+				op(data[i], args...);
+			return *this;
+		}
+		
+		template<typename T, typename... Args>
+		inline ByteReader& op(std::vector<T>& arr, Args... args) {
+			uint32_t bytes;
+			op(bytes);
+			arr.resize(bytes);
+			return op<T, Args...>(arr.data(), bytes, args...);
+		}
+		
+		template<typename T, typename... Args>
+		inline ByteReader& op(std::set<T>& set, Args... args) {
+			uint32_t bytes;
+			op(bytes);
+			for(uint32_t i=0; i<bytes; ++i) {
+				T v;
+				op(v, args...);
+				set.emplace_hint(set.end(), std::move(v));
+			}
+			return *this;
+		}
+		
+		template<typename T, typename... Args>
+		inline ByteReader& op(std::unordered_set<T>& set, Args... args) {
+			uint32_t bytes;
+			op(bytes);
+			for(uint32_t i=0; i<bytes; ++i) {
+				T v;
+				op(v, args...);
+				set.insert(std::move(v));
+			}
+			return *this;
+		}
+		
+		inline bool is_valid() const {
+			return offset <= size;
+		}
+		
+		inline bool has_any_more() const {
+			return offset < size;
+		}
+		
+	private:
+		
+		const uint8_t* buffer;
+		const uint32_t size;
+		uint32_t offset;
+// 		
+// 	private:
+// 		
+// 		template<> ByteReader& op<uint8_t >(uint8_t& ) = delete;
+// 		template<> ByteReader& op<uint16_t>(uint16_t&) = delete;
+// 		template<> ByteReader& op<uint32_t>(uint32_t&) = delete;
+// 		template<> ByteReader& op<uint64_t>(uint64_t&) = delete;
+// 		template<> ByteReader& op<int8_t  >(int8_t&  ) = delete;
+// 		template<> ByteReader& op<int16_t >(int16_t& ) = delete;
+// 		template<> ByteReader& op<int32_t >(int32_t& ) = delete;
+// 		template<> ByteReader& op<int64_t >(int64_t& ) = delete;
+// 		
+// 		template<> ByteReader& op<uint8_t , uint32_t>(uint8_t& , uint32_t) = delete;
+// 		template<> ByteReader& op<uint16_t, uint32_t>(uint16_t&, uint32_t) = delete;
+// 		template<> ByteReader& op<uint32_t, uint32_t>(uint32_t&, uint32_t) = delete;
+// 		template<> ByteReader& op<uint64_t, uint32_t>(uint64_t&, uint32_t) = delete;
+// 		template<> ByteReader& op<int8_t  , uint32_t>(int8_t&  , uint32_t) = delete;
+// 		template<> ByteReader& op<int16_t , uint32_t>(int16_t& , uint32_t) = delete;
+// 		template<> ByteReader& op<int32_t , uint32_t>(int32_t& , uint32_t) = delete;
+// 		template<> ByteReader& op<int64_t , uint32_t>(int64_t& , uint32_t) = delete;
+// 		
+// 		template<> ByteReader& op<int >(int& ) = delete;
+// 		template<> ByteReader& op<char >(char& ) = delete;
+// 		template<> ByteReader& op<short >(short& ) = delete;
+// 		template<> ByteReader& op<long >(long& ) = delete;
+// 		template<> ByteReader& op<long long >(long long& ) = delete;
+// 		template<> ByteReader& op<size_t >(size_t& ) = delete;
+// 		
+// 		template<> ByteReader& op<int, uint32_t>(int&, uint32_t) = delete;
+// 		template<> ByteReader& op<char, uint32_t>(char&, uint32_t) = delete;
+// 		template<> ByteReader& op<short, uint32_t>(short&, uint32_t) = delete;
+// 		template<> ByteReader& op<long, uint32_t>(long&, uint32_t) = delete;
+// 		template<> ByteReader& op<long long, uint32_t>(long long&, uint32_t) = delete;
+// 		template<> ByteReader& op<size_t, uint32_t>(size_t&, uint32_t) = delete;
+// 		
+// 		template<> ByteReader& op<float>(float& value) = delete;
+// 		template<> ByteReader& op<double>(double& value) = delete;
+// 		template<> ByteReader& op<float,float,float,uint32_t>(float& value, float min, float max, uint32_t bytes) = delete;
+// 		template<> ByteReader& op<double,double,double,uint32_t>(double& value, double min, double max, uint32_t bytes) = delete;
+// 		template<> ByteReader& op<float,float,float,float,uint32_t>(float& value, float offset, float min, float max, uint32_t bytes) = delete;
+// 		template<> ByteReader& op<double,double,double,double,uint32_t>(double& value, double offset, double min, double max, uint32_t bytes) = delete;
+// 		
+// 		template<> ByteReader& op<std::string>(std::string& str) = delete;
+// 		template<> ByteReader& op<std::vector<uint8_t>>(std::vector<uint8_t>& binary) = delete;
+	};
+	
+	
+	
+	inline ByteReader& ByteReader::op(std::string& str) {
+		str.clear();
+		str = (char*)buffer+offset;
+		offset += str.size()+1;
+		return *this;
+	}
+	
+	inline ByteReader& ByteReader::op(uint8_t* data, uint32_t bytes) {
+		memcpy(data, buffer+offset, bytes);
+		offset += bytes;
+		return *this;
+	}
+	
+	inline ByteReader& ByteReader::op(int8_t* data, uint32_t bytes) {
+		return op((uint8_t*)data, bytes);
+	}
+	
+	
+	inline ByteReader& ByteReader::op(std::vector<uint8_t>& data) {
+		uint32_t bytes;
+		op(bytes);
+		data.resize(bytes);
+		return this->op((uint8_t*)&(data.front()), bytes);
+	}
+	
+	
+	
+	inline ByteReader& ByteReader::op(uint8_t& v,  uint32_t bytes) {
+		v = buffer[offset];
+		offset++;
+		return *this;
+	}
+	inline ByteReader& ByteReader::op(uint16_t& v, uint32_t bytes) {
+		v = 0;
+		for(int i=0; i<bytes; ++i)
+			v |= (buffer[offset++])<<(i<<3);
+		return *this;
+	}
+	inline ByteReader& ByteReader::op(uint32_t& v, uint32_t bytes) {
+		v = 0;
+		for(int i=0; i<bytes; ++i)
+			v |= (buffer[offset++])<<(i<<3);
+		return *this;
+	}
+	inline ByteReader& ByteReader::op(uint64_t& v, uint32_t bytes) {
+		v = 0;
+		for(int i=0; i<bytes; ++i)
+			v |= (buffer[offset++])<<(i<<3);
+		return *this;
+	}
+	
+	inline ByteReader& ByteReader::op(int8_t& v,   uint32_t bytes) { return op(v); }
+	inline ByteReader& ByteReader::op(int16_t& v,  uint32_t bytes) { return op((uint16_t&)v); }
+	inline ByteReader& ByteReader::op(int32_t& v,  uint32_t bytes) { return op((uint32_t&)v, bytes); }
+	inline ByteReader& ByteReader::op(int64_t& v,  uint32_t bytes) { return op((uint64_t&)v, bytes); }
+	
+	
+	
+	inline ByteReader& ByteReader::op(uint8_t& v)  { return op(v, (uint32_t)1); }
+	inline ByteReader& ByteReader::op(uint16_t& v) { return op(v, (uint32_t)2); }
+	inline ByteReader& ByteReader::op(uint32_t& v) { return op(v, (uint32_t)4); }
+	inline ByteReader& ByteReader::op(uint64_t& v) { return op(v, (uint32_t)8); }
+	
+	inline ByteReader& ByteReader::op(int8_t& v)  { return op((uint8_t&)v); }
+	inline ByteReader& ByteReader::op(int16_t& v) { return op((uint16_t&)v); }
+	inline ByteReader& ByteReader::op(int32_t& v) { return op((uint32_t&)v); }
+	inline ByteReader& ByteReader::op(int64_t& v) { return op((uint64_t&)v); }
+	
+	
+	
+	inline ByteReader& ByteReader::op(float& value) {
+		return op(*(uint32_t*)&value, 4);
+	}
+	
+	inline ByteReader& ByteReader::op(double& value) {
+		return op(*(uint64_t*)&value, 8);
+	}
+	
+	inline ByteReader& ByteReader::op(float& value, float min, float max,
+			uint32_t bytes) {
+		float fmask = (((uint64_t)1)<<(bytes<<3))-1ll;
+		uint64_t v = 0;
+		return op(v, bytes);
+		value = v * (max-min)/fmask;
+	}
+	
+	inline ByteReader& ByteReader::op(double& value, double min, double max,
+			uint32_t bytes) {
+		double fmask = (((uint64_t)1)<<(bytes<<3))-1ll;
+		uint64_t v = 0;
+		return op(v, bytes);
+		value = v * (max-min)/fmask;
+	}
+	
+	inline ByteReader& ByteReader::op(float& value, float offset, float min,
+			float max, uint32_t bytes) {
+		op(value, min, max, bytes);
+		value -= offset;
+		return *this;
+	}
+	
+	inline ByteReader& ByteReader::op(double& value, double offset, double min,
+			double max, uint32_t bytes) {
+		op(value, min, max, bytes);
+		value -= offset;
+		return *this;
+	}
+	
+} // namespace bitscpp
+
+#endif
+
---
+++ tests/Test.cpp
@@ -0,0 +1,188 @@
+
+#include <bitscpp/Writer.hpp>
+#include <bitscpp/Reader.hpp>
+
+#include <iostream>
+
+#include <cstdio>
+
+// #pragma pack(push, 1)
+struct Struct {
+	Struct() {
+		clear();
+	}
+	
+	Struct& operator= (const Struct& other) {
+		memcpy(this, &other, sizeof(*this));
+		return *this;
+	}
+	
+	bool operator==(const Struct& other) {
+		return !memcmp(this, &other, sizeof(*this));
+	}
+	
+	uint32_t bytes16;
+	uint32_t bytes32;
+	uint32_t bytes64;
+	
+	uint8_t  a8;
+	int8_t  ua8;
+	uint8_t  b8;
+	int8_t  ub8;
+	
+	uint16_t a16;
+	int16_t ua16;
+	uint16_t b16;
+	int16_t ub16;
+	
+	uint32_t a32;
+	int32_t ua32;
+	uint32_t b32;
+	int32_t ub32;
+	
+	uint64_t a64;
+	int64_t ua64;
+	uint64_t b64;
+	int64_t ub64;
+	
+	double f2;
+	float f1;
+	
+	void cmp(Struct& s) {
+		std::cout << "\n\n";
+		std::cout << " bytes counts\n";
+		std::cout << bytes16 	<< "    " << s.bytes16 << "\n";
+		std::cout << bytes32 	<< "    " << s.bytes32 << "\n";
+		std::cout << bytes64 	<< "    " << s.bytes64 << "\n";
+		std::cout << " ints 8\n";
+		std::cout << (int)a8 	<< "    " << (int)s.a8 << "\n";
+		std::cout << (int)ua8 	<< "    " << (int)s.ua8 << "\n";
+		std::cout << (int)b8 	<< "    " << (int)s.b8 << "\n";
+		std::cout << (int)ub8 	<< "    " << (int)s.ub8 << "\n";
+		std::cout << " ints 16\n";
+		std::cout << a16 		<< "    " << s.a16 << "\n";
+		std::cout << ua16 		<< "    " << s.ua16 << "\n";
+		std::cout << b16 		<< "    " << s.b16 << "\n";
+		std::cout << ub16 		<< "    " << s.ub16 << "\n";
+		std::cout << " ints 32\n";
+		std::cout << a32 		<< "    " << s.a32 << "\n";
+		std::cout << ua32 		<< "    " << s.ua32 << "\n";
+		std::cout << b32 		<< "    " << s.b32 << "\n";
+		std::cout << ub32 		<< "    " << s.ub32 << "\n";
+		std::cout << " ints 64\n";
+		std::cout << a64 		<< "    " << s.a64 << "\n";
+		std::cout << ua64 		<< "    " << s.ua64 << "\n";
+		std::cout << b64 		<< "    " << s.b64 << "\n";
+		std::cout << ub64 		<< "    " << s.ub64 << "\n";
+		std::cout << " floats\n";
+		std::cout << f2 		<< "    " << s.f2 << "\n";
+		std::cout << f1 		<< "    " << s.f1 << "\n";
+	}
+	
+	template<typename S>
+	S& __ByteStream_op(S& s) {
+		s.op(bytes16, 1);
+		s.op(bytes32, 1);
+		s.op(bytes64, 1);
+		
+		s.op(a8);
+		s.op(ua8);
+		s.op(a16, bytes16);
+		s.op(ua16, bytes16);
+		s.op(a32, bytes32);
+		s.op(ua32, bytes32);
+		s.op(a64, bytes64);
+		s.op(ua64, bytes64);
+		
+		s.op(b8);
+		s.op(ub8);
+		s.op(b16);
+		s.op(ub16);
+		s.op(b32);
+		s.op(ub32);
+		s.op(b64);
+		s.op(ub64);
+		
+		s.op(f1);
+		s.op(f2);
+		return s;
+	}
+	
+	void clear() {
+		memset(this, 0, sizeof(*this));
+	}
+};
+// #pragma pack(pop)
+
+static uint64_t I = 1;
+void Random(void* ptr, uint32_t bytes) {
+	static uint64_t I = 1;
+	for(int i=0; i<bytes; ++i) {
+		((uint8_t*)ptr)[i] = ++I;
+	}
+}
+
+void Random(Struct& s) {
+	static int _b16 = -1;
+	static int _b32 = -1;
+	static int _b64 = -1;
+	
+	s.clear();
+	
+	s.a8 = ++I;
+	s.ua8 = ++I;
+	s.b8 = ++I;
+	s.ub8 = ++I;
+	s.a16 = ++I;
+	s.ua16 = ++I;
+	s.b16 = ++I;
+	s.ub16 = ++I;
+	s.a32 = ++I;
+	s.ua32 = ++I;
+	s.b32 = ++I;
+	s.ub32 = ++I;
+	s.a64 = ++I;
+	s.ua64 = ++I;
+	s.b64 = ++I;
+	s.ub64 = ++I;
+	s.f2 = ++I;
+	s.f1 = ++I;
+	
+	s.bytes16 = ((++_b16)&1) + 1;
+	s.bytes32 = ((++_b32)&2) + 1;
+	s.bytes64 = ((++_b64)&3) + 1;
+		
+	s.a16 &=  (1llu<<(s.bytes16<<3)) - 1;
+	s.ua16 &= (1llu<<(s.bytes16<<3)) - 1;
+	s.a32 &=  (1llu<<(s.bytes32<<3)) - 1;
+	s.ua32 &= (1llu<<(s.bytes32<<3)) - 1;
+	s.a64 &=  (1llu<<(s.bytes64<<3)) - 1;
+	s.ua64 &= (1llu<<(s.bytes64<<3)) - 1;
+}
+
+int main() {
+	
+	std::vector<uint8_t> buffer;
+	
+	for(int i=0; i<1; ++i) {
+		buffer.resize(10000);
+		Random((void*)buffer.data(), buffer.size());
+		buffer.clear();
+		
+		Struct s1, s2;
+		s1.clear();
+		s2.clear();
+		Random(s1);
+		
+		bitscpp::ByteWriter writer(buffer);
+		writer.op(s1);
+		
+		bitscpp::ByteReader reader(buffer.data(), buffer.size());
+		reader.op(s2);
+		
+		printf(" equality: %i -> %s\n", i, s1==s2 ? "true" : "false");
+		s1.cmp(s2);
+	}
+	return 0;
+}
+
